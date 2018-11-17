119c8774c900cf417a03a206d375207ddfbc8776
David Wang <davidcwang210@gmail.com>
2018-11-17 00:49:48 -0800

    Added 763_Partition_Labels and 861_Score_After_Flipping_Matrix/

---
+++ 861_Score_After_Flipping_Matrix/solution.py
@@ -0,0 +1,19 @@
+"""
+Problem: 861 Score After Flipping Matrix 
+Url: https://leetcode.com/problems/score-after-flipping-matrix/ 
+Author: David Wang
+Date: 11/3/2018
+"""
+class Solution(object):
+    def matrixScore(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: int
+        """
+        [[0,0,1,1],
+         [1,0,1,0],
+         [1,1,0,0]]
+
+        [[1,1,1,1],
+         [1,0,0,1],
+         [1,1,1,1]]
