6011d8b2b1a7c60d5814f55ee9c631d78fa78089
David Wang <davidcwang210@gmail.com>
2019-01-28 11:29:51 -0800

    Partially solved 684_Redundant_Connection

--- 684_Redundant_Connection/solution.py
+++ 684_Redundant_Connection/solution.py
@@ -8,8 +8,9 @@ Date: 01/18/2019
 import sys
 
 class Solution(object):
-    self.rank = {}
-    self.root= {}
+    self.rank = [0] * 1000
+    self.root= range(1000) 
+
     def findRedundantConnection(self, edges):
         """
         :type edges: List[List[int]]
@@ -18,8 +19,9 @@ class Solution(object):
         redun = None
         for e in edges:
             src, dest = e
-            srcr, srcpar = get_root(src)
-            destr, destpar = get_root(dest)
+            srcr = find(src)
+            destr = find(dest)
+
             if rank[srcr] > rank[destr]:
                 self.root[destr] = srcr
                 self.root = map(lambda i: self.root[i] = destr, srcrpar)
@@ -31,12 +33,7 @@ class Solution(object):
                 self.root = map(lambda i: self.root[i] = destr, srcrpar)
                 self.rank[destr] += len(srcrpar)
 
-
-    def get_root(self, i, parents=[]):
-        if self.root[i] == i:
-            return i, parents
-        parents.append(i)
-        get_root(self.root[i], parents)
-
-    def union(
-
+    def find(self, x):
+        if self.root[x] != x:
+            self.root[x] = find(self.root[x])
+        return x 
