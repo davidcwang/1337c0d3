4581f1ad7a4b410c281790ad83009935ff506830
David Wang <davidwang@davids-mbp.lan>
2020-05-26 17:12:42 -0700

    759_Employee_Free_Time

---
+++ 289_Game_of_Life/solution.py
@@ -0,0 +1,13 @@
+"""
+Problem: 4. Median of Two Sorted Arrays
+Url: https://leetcode.com/problems/median-of-two-sorted-arrays/
+Author: David Wang
+Date: 05/19/2020
+"""
+
+from types import List
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int],
+        nums2: List[int]) -> float:
+        
